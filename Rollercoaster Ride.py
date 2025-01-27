eae931862745e20b8d2b4be58ab5ed925ec03244
Vezstorm <187674837+Vezstorm@users.noreply.github.com>
2025-01-27 20:01:38 +0400

    Update Rollercoaster Ride.py

--- Rollercoaster Ride.py
+++ Rollercoaster Ride.py
@@ -1,3 +1,10 @@
+# Task Requirements
+# Collect the users height and age information to check to see if they can ride the rollercoaster
+# If their height isnt 120cm say "Sorry you have to grow taller before you can ride"
+# If their height 120cm and they are 18 years old say "The ticket will cost you 120 OMR"
+# If their height is 120cm and they are 120 years old say "The ticket will cost you 5 OMR"
+# If their height is 120cm and they are in the ages 12 to 18 say "The ticket will be 7 OMR"
+
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 age = int(input("What is your age? "))
@@ -13,7 +20,7 @@ if height >= 120:
 else:
     print("Sorry you have to grow taller before you can ride.")
 
-
+# Notes
 # We can use elif as many times as we like
 # A nested if statement would be written inside the original if statement with and is indented
 # An Elif statement would be written inside the nested if statement and does not require and else
