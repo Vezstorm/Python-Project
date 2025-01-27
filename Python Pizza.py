6e6ecd0c25fcbfd761f019655b336fa8abf2937a
Vezstorm <187674837+Vezstorm@users.noreply.github.com>
2025-01-27 19:32:33 +0400

    Python Pizza.py

--- Python Pizza.py
+++ Python Pizza.py
@@ -1,3 +1,12 @@
+# Task Requirements:
+# Based on a user's order, work out their final bill. Use the input() function to get a user's preferences and then add up the total for their order and tell them how much they have to pay.
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
@@ -30,4 +39,4 @@ if extra_cheese == "Y":
 # Prints out the final result of the price
 print(f"your final bill is: ${price}.")
 
-# She did not teach about being able to use logical operators in if statements
+
