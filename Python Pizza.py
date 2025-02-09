6611a4e8446a1751b8d26d437a043302be275ab4
Vezstorm <187674837+Vezstorm@users.noreply.github.com>
2025-02-09 10:34:03 +0400

    Python Pizza.py

--- Python Pizza.py
+++ Python Pizza.py
@@ -7,36 +7,39 @@
 # Add pepperoni for medium or large pizza (Y or N): +$3
 # Add extra cheese for any size pizza (Y or N): +$1
 
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
-
 # todo: Work out how much they need to pay based on their choice
 # todo: Work out how much to add to their bill based on their pepperoni choice
 # todo: Work out their final amount based on whether they want to add cheese
 
-# Considers what type of pizza is selected
-if size == "S":
-    price = 15
-if size == "M":
-    price = 20
-if size == "L":
-    price = 25
+print("Welcome to Python Pizza!")
+size = input("Please type a size from the following options, S, M, L")
+pepperoni = input("Would you like to add pepperoni? Type Y or N")
+extra_cheese = input("Would you like to add extra cheese? Type Y or N")
 
-# Checks to see if they added pepperoni and to add up the cost if its small its +2 and if its medium or large its +3 to the final price
-if pepperoni == "Y":
-    if size == "S":
-        price = price + 2
-    elif size in ["M", "L"]:
-        price = price + 3
+bill = 0
 
+# Size
+if size.upper() == "S":
+    bill =  15
+elif size.upper() == "M":
+    bill  = 20
+elif size.upper() == "L":
+    bill = 25
+else:
+    print("Please type one of the three options so we can calculate your final bill.")
 
-# Checks to see if the user added extra_cheese and adds up the cost to the final price
-if extra_cheese == "Y":
-    price = price + 1
+# Pepperoni
+if size.upper() == "S" and pepperoni.upper() == "Y":
+    bill = bill + 2
+if size.upper() in ['M', 'L'] and pepperoni.upper() == "Y":
+    bill = bill + 3
 
-# Prints out the final result of the price
-print(f"your final bill is: ${price}.")
+# Extra Cheese
+if extra_cheese.upper() == "Y":
+    bill = bill + 1
+    
+
+print(f"Your final bill is ${bill}.")
+
 
 
